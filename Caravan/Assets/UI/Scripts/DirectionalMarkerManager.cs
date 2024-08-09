df68127c9265b9a46c11f2d16264791d1b3044fa
Randelladie <100122826+Randelladie@users.noreply.github.com>
2024-08-10 01:55:16 +0800

    Update Fix to be able to create Standalone

--- Caravan/Assets/UI/Scripts/DirectionalMarkerManager.cs
+++ Caravan/Assets/UI/Scripts/DirectionalMarkerManager.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Sprites;
+//using UnityEditor.Sprites;
 using UnityEngine;
 
 public class DirectionalMarkerManager : MonoBehaviour
